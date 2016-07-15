10350850895907ddedaab4f58ff1c30cade802d5
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-07-14 20:40:26 -0400

    Add js to add a paragraph when the header of article 1 is clicked

--- js/main.js
+++ js/main.js
@@ -7,3 +7,15 @@ function changeImage() {
 }
 
 image.addEventListener('mouseover', changeImage);
+
+//This event listens for a click on the first article's h2 and then //adds a paragraph to the end of the article.
+
+var firstArticle = document.getElementById('firstArticleHead');
+
+function addParagraph() {
+    var newElement = document.createElement('p');
+    newElement.innerHTML = '<p>Cupcake ipsum dolor sit amet danish powder topping carrot cake. Dragée cake jelly chocolate candy canes chocolate bar sugar plum gingerbread. Chocolate bar macaroon danish sesame snaps chocolate bar carrot cake jelly beans icing marzipan.</p>';
+    document.getElementsByClassName('articles')[0].appendChild(newElement);
+}
+
+firstArticle.addEventListener('click', addParagraph);
